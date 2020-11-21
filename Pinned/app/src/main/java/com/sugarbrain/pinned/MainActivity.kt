d26272c372556eb25c5e22833b078710b1515052
eduardosm7 <esm7@cin.ufpe.br>
2020-11-21 18:12:11 -0300

    [feature/feed-activity] Create feed activity

--- Pinned/app/src/main/java/com/sugarbrain/pinned/MainActivity.kt
+++ Pinned/app/src/main/java/com/sugarbrain/pinned/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.sugarbrain.pinned
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.sugarbrain.pinned.feed.FeedActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var feedButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createFeedButtonListener()
+    }
+
+    private fun createFeedButtonListener() {
+        feedButton = findViewById(R.id.main_feed_button)
+        feedButton.setOnClickListener {
+            val feedIntent = Intent(this, FeedActivity::class.java);
+            startActivity(feedIntent);
+        }
     }
 }

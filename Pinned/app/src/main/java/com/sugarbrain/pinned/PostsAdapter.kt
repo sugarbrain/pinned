9129a5ebcc123fcfc289387612cb962e108c4c5e
gilesv <gilesv@protonmail.com>
2020-11-25 22:10:01 -0300

    Add Post list in PostActivity

---
+++ Pinned/app/src/main/java/com/sugarbrain/pinned/PostsAdapter.kt
@@ -0,0 +1,43 @@
+package com.sugarbrain.pinned
+
+import android.content.Context
+import android.text.Html
+import android.text.Html.FROM_HTML_MODE_LEGACY
+import android.text.Spanned
+import android.text.format.DateUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.sugarbrain.pinned.models.Post
+import kotlinx.android.synthetic.main.item_post.view.*
+
+class PostsAdapter (val context: Context, val posts: List<Post>) :
+    RecyclerView.Adapter<PostsAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_post, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = posts.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(posts[position])
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(post: Post) {
+            val desc = context.getString(
+                R.string.post_description,
+                post.user?.arroba,
+                post.description
+            )
+
+            itemView.tvDescription.text = desc
+            itemView.tvDate.text = DateUtils.getRelativeTimeSpanString(post.date)
+            Glide.with(context).load(post.imageUrl).into(itemView.imageView)
+        }
+    }
+}
--- Pinned/app/src/main/java/com/sugarbrain/pinned/feed/FeedActivity.kt
+++ Pinned/app/src/main/java/com/sugarbrain/pinned/feed/FeedActivity.kt
@@ -3,19 +3,63 @@ package com.sugarbrain.pinned.feed
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import com.sugarbrain.pinned.PostsAdapter
 import com.sugarbrain.pinned.R
+import com.sugarbrain.pinned.models.Post
 import com.sugarbrain.pinned.search.SearchActivity
+import kotlinx.android.synthetic.main.activity_feed.*
+
+private const val TAG = "FeedActivity"
 
 class FeedActivity : AppCompatActivity() {
 
     private lateinit var searchEditText: EditText;
 
+    private lateinit var firestoreDb: FirebaseFirestore
+    private lateinit var posts: MutableList<Post>
+    private lateinit var adapter: PostsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feed)
         createSearchEditTextListener()
+
+
+        firestoreDb = FirebaseFirestore.getInstance()
+
+        // Create the layout file which represents one post - DONE
+        // Create data source - DONE
+        posts = mutableListOf()
+        // Create the adapter
+        adapter = PostsAdapter(this, posts)
+
+        // Bind the adapter and layout manager to the RV
+        rvPosts.adapter = adapter
+        rvPosts.layoutManager = LinearLayoutManager(this)
+
+        val postsReference = firestoreDb
+            .collection("posts")
+            .limit(20)
+            .orderBy("date", Query.Direction.DESCENDING)
+
+        postsReference.addSnapshotListener { snapshot, exception ->
+            if (exception != null || snapshot == null) {
+                Log.e(TAG, "Exception when querying posts", exception)
+                return@addSnapshotListener
+            }
+
+            val postList = snapshot.toObjects(Post::class.java)
+            posts.clear()
+            posts.addAll(postList)
+            adapter.notifyDataSetChanged()
+        }
     }
 
     private fun createSearchEditTextListener() {

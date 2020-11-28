f430767e6766831b7aaa89fda6243ecd661101bd
eduardosm7 <esm7@cin.ufpe.br>
2020-11-28 15:29:55 -0300

    [feature/profile] Profile

--- Pinned/app/src/main/java/com/sugarbrain/pinned/search/SearchHeader.kt
+++ Pinned/app/src/main/java/com/sugarbrain/pinned/search/SearchHeader.kt
@@ -3,17 +3,39 @@ package com.sugarbrain.pinned.search
 import android.content.Context
 import android.content.Intent
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.sugarbrain.pinned.R
+import com.sugarbrain.pinned.models.User
 import com.sugarbrain.pinned.profile.ProfileActivity
+import com.sugarbrain.pinned.submit.SubmitActivity
 import kotlinx.android.synthetic.main.search_header.view.*
 
 
 class SearchHeader(context: Context?, attrs: AttributeSet?) : ConstraintLayout(context, attrs) {
 
+    private var firestoreDb: FirebaseFirestore
+    private var currentUser: User? = null
+
     init {
         LayoutInflater.from(context).inflate(R.layout.search_header, this, true)
+
+        firestoreDb = FirebaseFirestore.getInstance()
+        firestoreDb.collection("users")
+            .document(FirebaseAuth.getInstance().currentUser?.uid as String)
+            .get()
+            .addOnSuccessListener { userSnapshot ->
+                currentUser = userSnapshot.toObject(User::class.java)
+                Glide.with(this).load(currentUser?.avatarUrl).into(profile_image)
+            }
+            .addOnFailureListener { exception ->
+                Log.i(SubmitActivity.TAG, "Fail to get current user", exception)
+            }
+
         setProfileImageOnClickListener()
     }
 
